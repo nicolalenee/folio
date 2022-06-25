feda4179ff9c20011fa514524a54bd1b4cffc300
cola-mars <86696492+cola-mars@users.noreply.github.com>
2022-06-25 17:27:24 -0400

    fill out modal information for each project

--- js/script.js
+++ js/script.js
@@ -1,23 +1,14 @@
-// get the modal
-var modal = document.getElementById("modal");
-// get the button to open the modal
-var btn = document.getElementById("modal-button");
-// get the element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-// when the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display="block";
-};
+$(document).on('click','.modal-button', function(){
+	var currentModal =  $(this).attr('href');
+	$('#modal').hide();
+	$('.modal-content').hide();
+	
+	$('#modal').fadeIn();
+	$(currentModal).fadeIn();
+  return false;
+});
 
-// display the modal details
-
-// when the user clicks on the x, close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-// when the user clicks outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+$("body" ).on( "click",".close", function() {
+  	$('#modal').fadeOut();
+	$('.modal-content').hide();
+});
